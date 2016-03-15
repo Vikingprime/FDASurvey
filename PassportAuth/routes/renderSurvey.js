d4858abc504b0ccc487bff29fd18e2b75092c080
Sanjid Halim <Sanjid Halim>
2016-03-15 13:09:20 -0500

    Added new survey creation

--- PassportAuth/routes/renderSurvey.js
+++ PassportAuth/routes/renderSurvey.js
@@ -4,13 +4,20 @@
 var express = require('express');
 var router = express.Router();
 var surveys = require('../models/surveyListModel');
+
 var query = undefined;
+var newSurvey = undefined;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     if(req.user) {
-        console.log("My paramter in renderSurvey.js is " + req.query.nm);
         query = req.query.nm;
+        newSurvey = false;
+        if (req.query.new == 'yes'){
+            query = 'yes';
+            newSurvey = true;
+            console.log(query);
+        }
         res.render('renderSurvey',{params: req.query.nm});
     }
     else {
@@ -19,29 +26,57 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/formData', function (req, res){
-/*    surveys.findOne({name: req.query.nm}, function (err, survey) {
-        var myObj =  survey.questions;*/
-
-    surveys.findOne({name: query}, function (err, survey) {
+    console.log(newSurvey);
+    if (newSurvey){
         var myObj = {};
-        myObj.questions = survey.questions;
-        myObj.editable = survey.editable;
-        myObj.name = survey.name;
-        myObj.id = survey._id;
+        myObj.questions = [];
+        myObj.editable = true;
+        myObj.name = null;
+        myObj.id = null;
         res.json(myObj);
-    })
+    }
+    else{
+        surveys.findOne({name: query}, function (err, survey) {
+            var myObj = {};
+            myObj.questions = survey.questions;
+            myObj.editable = survey.editable;
+            myObj.name = survey.name;
+            myObj.id = survey._id;
+            res.json(myObj);
+        })
+    }
 });
 
 router.post('/saveSurvey', function(req,res){
-    console.log("in body");
+    console.log("in saveSurvey");
     console.log(req.body);
-    surveys.update({_id:req.body.id}, { $set: {
-        questions: req.body.questions,
-        editable: req.body.editable,
-        name: req.body.name
-    }}, {}, function(err, numAffected){
-        console.log("Updated and Num Affected " + numAffected);
-        res.send(err);
-    })
+
+    if (req.body.id !=null) {
+        console.log("ID is not null")
+        surveys.update({_id: req.body.id}, {
+            $set: {
+                questions: req.body.questions,
+                editable: req.body.editable,
+                name: req.body.name
+            }
+        }, {}, function (err, numAffected) {
+            console.log("Updated and Num Affected " + numAffected);
+            res.send(err);
+        })
+    } else {
+        console.log("Insertion Error");
+        var survey = new surveys({
+            questions: req.body.questions,
+            editable: req.body.editable,
+            name: req.body.name,
+            username: req.user.username
+        })
+        survey.save(function(err, surv){if (err) console.log("Error :" + error )});
+/*        surveys.insert({
+                questions: "abc",
+                editable: "def",
+                name: "dgi"
+            });*/
+    }
 });
 module.exports = router;
