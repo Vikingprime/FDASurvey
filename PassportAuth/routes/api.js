18cf63855880014fedc786cbeec50c13b466d7e2
Vikas <vikas.s.pydah@vanderbilt.edu>
2016-03-15 19:45:38 +0000

    api updates

--- PassportAuth/routes/api.js
+++ PassportAuth/routes/api.js
@@ -5,7 +5,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/getSurvey', function(req, res, next) {
-    res.end('Test  string');
+    var x = { questions:
+   [ { prompt: 'how smelly are you?' },
+     { prompt: 'how fat are you?' },
+     { prompt: 'what\'s your favorite tv show?' } ]
+   };
+//var headers = req.getAllResponseHeaders().toLowerCase();
+//    console.log("Email addr:" +headers);
+    console.log(req.headers.email);
+    res.json(x);
 });
 
 module.exports = router;
